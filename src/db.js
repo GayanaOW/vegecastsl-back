af1d6c8892078d2c2c5411d0db281c4b55665687
GayanaOW <gayana.waraketiya@gmail.com>
2026-03-24 14:00:48 +0530

    set public MYSQL URL

--- src/db.js
+++ src/db.js
@@ -1,16 +1,18 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
 
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT || 3306),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 // Test connection once at startup
 (async () => {
